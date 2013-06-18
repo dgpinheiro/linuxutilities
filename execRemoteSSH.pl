9537faad181d936811000905e1e3d39b42be1198
dgpinheiro <dgpinheiro@gmail.com>
2013-06-18 20:29:58 -0300

    Change from Net::SSH::Perl to Net::SSH::Expect and add some new parameters

--- execRemoteSSH.pl
+++ execRemoteSSH.pl
@@ -68,7 +68,8 @@ use warnings;
 use Readonly;
 use Getopt::Long;
 
-use Net::SSH::Perl;
+#use Net::SSH::Perl;
+use Net::SSH::Expect;
 use Term::ReadKey;
 use Net::IP;
 
@@ -80,22 +81,25 @@ INIT {
     $LOGGER = Log::Log4perl->get_logger($0);
 }
 
-my ($level, $ipdef, $cmd, $outdir, $prefix, $auxfile, $user, $password);
+my ($level, $ipdef, %cmdh, $outdir, $prefix, $auxfile, $user, $password, $expectpasswd);
 
+my ($default_login_output);
 Usage("Too few arguments") if $#ARGV < 0;
 GetOptions( "h|?|help" => sub { &Usage(); },
             "l|level=s"=> \$level,
             "i|ip=s"=>\$ipdef,
-            "c|command=s"=>\$cmd,
+            "c|command=s%"=>\%cmdh,
             "o|outdir=s"=>\$outdir,
             "p|prefix=s"=>\$prefix,
             "a|auxfile=s"=>\$auxfile,
-            "u|user=s"=>\$user
+            "u|user=s"=>\$user,
+            "e|expectpasswd"=>\$expectpasswd,
+            "n|loginout=s"=>\$default_login_output
     ) or &Usage();
 
 $prefix||='info_';
 $outdir||='.';
-
+$default_login_output||='Welcome';
 
 if ($level) {
     my %LEVEL = (   
@@ -112,7 +116,7 @@ if ($level) {
 }
 
 $LOGGER->logdie("Missing ip definition") unless ($ipdef);
-$LOGGER->logdie("Missing command to execute") unless ($cmd);
+$LOGGER->logdie("Missing command to execute") unless (scalar(keys %cmdh));
 
 unless (-d $outdir) {
     $LOGGER->logdie("Wrong output directory ($outdir)") unless (-d $outdir);
@@ -131,7 +135,6 @@ if ($auxfile) {
 } 
 else {
     $LOGGER->logdie("Missing user") unless ($user);
-    
     print "Please input your password: ";                                                                                                                                                                        
     ReadMode(4); #Disable the control keys
     my $key = 0;
@@ -166,20 +169,64 @@ foreach my $ipd ( split(',', $ipdef) ) {
     do {
         $LOGGER->logdie("Not found authentication for ".$ip->ip()."\n") unless ((exists $AUTH{$ip})||(exists $AUTH{'*'}));
         print $ip->ip(), "\n";
-        my $ssh = Net::SSH::Perl->new($ip->ip());
+        #my $ssh = Net::SSH::Perl->new($ip->ip());
+
+
+
         my ($u, $p) = ((exists $AUTH{$ip}) ? @{$AUTH{$ip}}{'user','password'} : @{$AUTH{'*'}}{'user','password'});
-        $ssh->login($u, $p);
-        my($stdout, $stderr, $exit) = $ssh->cmd($cmd);
-        if ($stdout) {
-            open(OUT, ">", $outdir.'/'.$prefix.$ip->ip().'_stdout.txt') or $LOGGER->logdie($!);
-            print OUT $stdout;
-            close(OUT);
+        
+        #$ssh->login($u, $p);
+        my $ssh = Net::SSH::Expect->new (
+            host => $ip->ip(), 
+            password=> $p, 
+            user => $u, 
+            raw_pty => 1,
+            exp_debug=>0,
+            restart_timeout_upon_receive=>1,
+            timeout=>5
+        );
+        my $login_output = $ssh->login();
+        if ($login_output !~ /$default_login_output/) {
+            $LOGGER->logdie("Login has failed. Login output was $login_output");
         }
-        if ($stderr) {    
-            open(ERR, ">", $outdir.'/'.$prefix.$ip->ip().'_stderr.txt') or $LOGGER->logdie($!);
-            print ERR $stderr;
-            close(ERR);
-        }        
+        foreach my $k (keys %cmdh) {
+            my $cmd = $cmdh{$k};
+            $ssh->exec("stty raw -echo");
+            #my ($stdout,$stderr) = $ssh->exec("$cmd");
+            my ($stdout,$stderr,$exit);
+
+            $ssh->send("$cmd");   # using send() instead of exec()
+            if ($expectpasswd) {
+                my $rwait = $ssh->waitfor('password for '.$user.':\s*\z', 1);
+                if ($rwait) {
+                    $ssh->send($password);
+                }
+                else {
+                    $LOGGER->logwarn("prompt 'password' not found after 1 second");
+                }                
+            }
+            my $line;
+            # returns the next line, removing it from the input stream:
+            while ( defined ($line = $ssh->read_line()) ) {
+                chomp($line);
+                if ($line) {
+                    $stdout.=$line . "\n";  
+                }
+            }
+            #($stdout, $stderr, $exit) = $ssh->cmd($cmd);
+                    
+            if ($stdout) {
+                open(OUT, ">", $outdir.'/'.$prefix.$ip->ip().'_'.$k.'_stdout.txt') or $LOGGER->logdie($!);
+                print OUT $stdout;
+                close(OUT);
+            }
+            if ($stderr) {    
+                open(ERR, ">", $outdir.'/'.$prefix.$ip->ip().'_'.$k.'_stderr.txt') or $LOGGER->logdie($!);
+                print ERR $stderr;
+                close(ERR);
+            }
+        }            
+        $ssh->close();
     } while ( ++$ip );
 }
 
@@ -199,14 +246,16 @@ Usage
 
 Argument(s)
 
-        -h      --help      Help
-        -l      --level     Log level [Default: FATAL]
-        -i      --ip        IP list (c.f. Net::IP - comma separated elements)
-        -c      --command   Remote command
-        -o      --outdir    Output directory (default: .)
-        -p      --prefix    Prefix (default: info_)
-        -a      --auxfile   Auxiliary file (IP<TAB>USER<TAB>PASSWORD)
-        -u      --user      Common user
+        -h      --help          Help
+        -l      --level         Log level [Default: FATAL]
+        -i      --ip            IP list (c.f. Net::IP - comma separated elements)
+        -c      --command       Remote command
+        -o      --outdir        Output directory (default: .)
+        -p      --prefix        Prefix (default: info_)
+        -a      --auxfile       Auxiliary file (IP<TAB>USER<TAB>PASSWORD)
+        -u      --user          Common user
+        -e      --expectpasswd  Command expect password
+        -n      --loginout      Default login output
 
 END_USAGE
     print STDERR "\nERR: $msg\n\n" if $msg;
